70d60e1188aed5b6dee3c8fadd3b79c63e6c9e51
yunjin-git <yunjin544@gmail.com>
2022-07-11 16:12:02 +0900

    First

---
+++ src/backup/publisher_node.cpp
@@ -0,0 +1,235 @@
+#include <stdio.h>
+#include <string.h>
+#include <inttypes.h>
+#include <sys/mman.h>
+#include <stdlib.h>
+#include <math.h>
+#include <unistd.h>
+#include <signal.h>
+#include <time.h>
+#include <alchemy/task.h>
+#include <alchemy/timer.h>
+#include "ethercat.h"
+
+#include "ros/ros.h"
+#include "std_msgs/String.h"
+
+#define CLOCK_RES 1e-9 
+#define EC_TIMEOUTMON 500
+#define LOOP_PERIOD 1e6
+#define NSEC_PER_SEC 1000000000
+
+char IOmap[256];
+int expectedWKC;
+int chk;
+volatile int wkc;
+OSAL_THREAD_HANDLE thread1;
+boolean LED_Control = TRUE;
+boolean needlf;
+boolean inOP;
+uint8 currentgroup = 0;
+int32 toff, gl_delta;
+RT_TASK loop_task;
+struct timespec time_stamp[2000];
+int tick[2000];
+char flag =0;
+
+void ECat_init(char *ifname, char *ifname2);
+void ECat_PDO_LOOP(void *arg);
+void Ecatcheck(void *ptr);
+
+int main(int argc, char **argv)
+{
+
+ros::init(argc,argv,"robot_Ecat_master");
+ros::NodeHandle n;
+ros::Publisher chatter_pub = n.advertise<std_msgs::String>("ros_communication", 1000);
+ros::Rate loop_rate(1000);
+
+ECat_init(argv[1],argv[2]);
+osal_thread_create(&thread1, 128000, (void*)&Ecatcheck, NULL);
+rt_task_create(&loop_task, "Ecat Loop", 0, 99, 0);
+rt_task_start(&loop_task, &ECat_PDO_LOOP, 0);
+int count = 0;
+int save_i = 0;
+ while (ros::ok())
+ {
+
+	std_msgs::String msg;
+	std::stringstream ss;
+	ss << (int)(ec_slave[0].inputs[0]) << "<-variable resistance level  "; 
+	msg.data = ss.str();
+
+	printf("%s\r", msg.data.c_str());
+	 
+   // if (flag == 1)
+   // {
+   //    FILE *fp;
+   //    fp = fopen("test.txt", "w");
+   //    for(save_i=0;save_i<=2000;save_i++)
+   //    {
+   //       long temp = time_stamp[save_i].tv_sec + time_stamp[save_i].tv_nsec;
+   //       fprintf(fp,"%ld,%d\n",temp,tick[save_i]);
+   //    }
+   // }
+	chatter_pub.publish(msg);
+	    ros::spinOnce();
+	   loop_rate.sleep();
+	++count;
+	}
+  return 0;
+}
+
+
+void ECat_init(char *ifname, char *ifname2)
+{
+
+   if (ec_init_redundant(ifname, ifname2))
+   {
+      printf("Starting Ecat DRCL Master Test\n");
+
+      if (ec_config_init(FALSE) > 0)
+      {
+         printf("%d slaves found and configured.\n", ec_slavecount);
+      }
+
+      ec_config_map(&IOmap);
+      printf("Slaves mapped, state to SAFE_OP.\n");
+      ec_statecheck(0, EC_STATE_SAFE_OP, EC_TIMEOUTSTATE * 4);
+
+      printf("segments : %d : %d %d %d %d\n", ec_group[0].nsegments, ec_group[0].IOsegment[0], ec_group[0].IOsegment[1], ec_group[0].IOsegment[2], ec_group[0].IOsegment[3]);
+      expectedWKC = (ec_group[0].outputsWKC * 2) + ec_group[0].inputsWKC;
+      printf("Calculated workcounter %d\n", expectedWKC);
+      ec_slave[0].state = EC_STATE_OPERATIONAL;
+      ec_send_processdata();
+      ec_receive_processdata(EC_TIMEOUTRET);
+      ec_writestate(0);
+      chk = 200;
+      ec_send_processdata();
+      ec_receive_processdata(EC_TIMEOUTRET);
+      ec_statecheck(0, EC_STATE_OPERATIONAL, 50000);
+      while (chk-- && (ec_slave[0].state != EC_STATE_OPERATIONAL))
+         ;
+   }
+}
+
+
+void ECat_PDO_LOOP(void *arg)
+{
+   RT_TASK *curtask;
+   rt_task_set_periodic(NULL, TM_NOW, LOOP_PERIOD);
+   unsigned int i = 0;
+   while (1)
+   {
+     /* Start Loop - Write your Realtime Program*/
+      wkc = ec_receive_processdata(EC_TIMEOUTRET);
+      // if(flag == 0 )
+      // {
+      //    clock_gettime(CLOCK_MONOTONIC, &time_stamp[i]);
+      //    if (i == 0)
+      //    {
+      //       tick[0]=0;
+      //    }
+      //    else
+      //    {
+      //       tick[i]=tick[i-1]+1;
+      //    }
+         
+      // if (tick[i] == 255)
+      // {
+      // tick[i]=0;
+      // }
+
+      // }
+
+      // if(i>2000)
+      // {
+      //    flag = 1;
+      // }
+      // i = i+1;
+
+      ec_slave[0].outputs[0] = !(ec_slave[0].outputs[0]);
+
+            
+      ec_send_processdata();
+      rt_task_wait_period(NULL);
+      
+      /* End Loop */
+   }
+}
+
+void Ecatcheck(void *ptr)
+{
+   int slave;
+   (void)ptr; /* Not used */
+
+   while (1)
+   {
+      if (inOP && ((wkc < expectedWKC) || ec_group[currentgroup].docheckstate))
+      {
+         if (needlf)
+         {
+            needlf = FALSE;
+            printf("\n");
+         }
+         /* one ore more slaves are not responding */
+         ec_group[currentgroup].docheckstate = FALSE;
+         ec_readstate();
+         for (slave = 1; slave <= ec_slavecount; slave++)
+         {
+            if ((ec_slave[slave].group == currentgroup) && (ec_slave[slave].state != EC_STATE_OPERATIONAL))
+            {
+               ec_group[currentgroup].docheckstate = TRUE;
+               if (ec_slave[slave].state == (EC_STATE_SAFE_OP + EC_STATE_ERROR))
+               {
+                  printf("ERROR : slave %d is in SAFE_OP + ERROR, attempting ack.\n", slave);
+                  ec_slave[slave].state = (EC_STATE_SAFE_OP + EC_STATE_ACK);
+                  ec_writestate(slave);
+               }
+               else if (ec_slave[slave].state == EC_STATE_SAFE_OP)
+               {
+                  printf("WARNING : slave %d is in SAFE_OP, change to OPERATIONAL.\n", slave);
+                  ec_slave[slave].state = EC_STATE_OPERATIONAL;
+                  ec_writestate(slave);
+               }
+               else if (ec_slave[slave].state > EC_STATE_NONE)
+               {
+                  if (ec_reconfig_slave(slave, EC_TIMEOUTMON))
+                  {
+                     ec_slave[slave].islost = FALSE;
+                     printf("MESSAGE : slave %d reconfigured\n", slave);
+                  }
+               }
+               else if (!ec_slave[slave].islost)
+               {
+                  /* re-check state */
+                  ec_statecheck(slave, EC_STATE_OPERATIONAL, EC_TIMEOUTRET);
+                  if (ec_slave[slave].state == EC_STATE_NONE)
+                  {
+                     ec_slave[slave].islost = TRUE;
+                     printf("ERROR : slave %d lost\n", slave);
+                  }
+               }
+            }
+            if (ec_slave[slave].islost)
+            {
+               if (ec_slave[slave].state == EC_STATE_NONE)
+               {
+                  if (ec_recover_slave(slave, EC_TIMEOUTMON))
+                  {
+                     ec_slave[slave].islost = FALSE;
+                     printf("MESSAGE : slave %d recovered\n", slave);
+                  }
+               }
+               else
+               {
+                  ec_slave[slave].islost = FALSE;
+                  printf("MESSAGE : slave %d found\n", slave);
+               }
+            }
+         }
+         if (!ec_group[currentgroup].docheckstate)
+            printf("OK : all slaves resumed OPERATIONAL.\n");
+      }
+   }
+}
